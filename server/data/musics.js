1af6e4506b0801fc1bda313dad0fa01c2d6ee563
pravashupreti <pravashupreti@gmail.com>
2022-04-19 10:27:03 -0500

    Add sample song

--- server/data/musics.js
+++ server/data/musics.js
@@ -1,51 +1,61 @@
 const musics = [{
         id: 1,
         title: "Blinding Lights",
-        releaseDate: '2019-01-01'
+        releaseDate: '2019-01-01',
+        fileName: 'binding-lights.mp3'
     }, {
         id: 2,
         title: "The Twist",
-        releaseDate: '1960-01-01'
+        releaseDate: '1960-01-01',
+        fileName: 'twist.mp3'
     },
     {
         id: 3,
         title: "Smooth",
-        releaseDate: '1960-01-01'
+        releaseDate: '1960-01-01',
+        fileName: 'smooth.mp3'
     },
     {
         id: 4,
         title: "Mack the Knife",
-        releaseDate: '1960-01-01'
+        releaseDate: '1960-01-01',
+        fileName: 'mack-the-knife.mp3'
     },
     {
         id: 5,
         title: "Uptown Funk",
-        releaseDate: '1960-01-01'
+        releaseDate: '1960-01-01',
+        fileName: 'uptown-funk.mp3'
     },
     {
         id: 6,
         title: "How Do I Live",
-        releaseDate: '1960-01-01'
+        releaseDate: '1960-01-01',
+        fileName: 'how-do-i-live.mp3'
     },
     {
         id: 7,
         title: "Party Rock Anthem",
-        releaseDate: '1960-01-01'
+        releaseDate: '1960-01-01',
+        fileName: 'party-rock-anthem.mp3'
     },
     {
         id: 8,
         title: "I Gotta Feeling",
-        releaseDate: '1960-01-01'
+        releaseDate: '1960-01-01',
+        fileName: ''
     },
     {
         id: 9,
         title: "Macarena (Bayside Boys mix)",
-        releaseDate: '1960-01-01'
+        releaseDate: '1960-01-01',
+        fileName: 'I-gotta-feeling.mp3'
     },
     {
         id: 10,
         title: "Shape of You",
-        releaseDate: '1960-01-01'
+        releaseDate: '1960-01-01',
+        fileName: 'shape-of-you.mp3'
     },
 
 ]
