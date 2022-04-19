e6b53c1e1453ff1e1ca798046c6d02f4866d0db4
pravashupreti <pravashupreti@gmail.com>
2022-04-19 13:56:48 -0500

    Add  seek and  timer features

--- client/js/main.js
+++ client/js/main.js
@@ -90,6 +90,7 @@ window.onload = function() {
         }
 
     }
+
     document.getElementById("nextSongBtn").onclick = function() {
 
         if (playIn == "SHUFFLE") {
@@ -134,8 +135,135 @@ window.onload = function() {
         playIn = "LOOP"
     }
 
+    document.getElementById('progressContainer').addEventListener('click', setProgressFn);
+
+
+    const audio = document.getElementById('audio');
+    audio.addEventListener('timeupdate', updateProgressFn);
+    audio.addEventListener('timeupdate', updateTimeDurationFn);
+
+    audio.addEventListener('ended', nextSongFn);
+
 
 }
+
+let nextSongFn = function(e) {
+
+    if (playIn == "SHUFFLE") {
+        playNewSong(getShuffledSong())
+        return
+    }
+    if (playIn == "LOOP") {
+
+        if (playingSong) {
+
+            let index = playlist.findIndex(x => x.id == playingSong.id)
+
+            index++
+            if (index < playlist.length) {
+                playNewSong(playlist[index].id)
+            } else {
+
+                playNewSong(playlist[0].id)
+            }
+
+        }
+        return
+    }
+
+    if (playIn == "REPEAT_ONCE") {
+        if (playingSong) {
+            playNewSong(playingSong.id)
+        }
+    }
+}
+
+let setProgressFn = function(e) {
+    const audio = document.getElementById('audio');
+
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
+
+let updateTimeDurationFn = function(e) {
+
+    const currTime = document.getElementById('currentTime');
+    const durTime = document.getElementById("timeDuration")
+
+    const { duration, currentTime } = e.srcElement;
+    var sec;
+    var sec_d;
+
+    // define minutes currentTime
+    let min = (currentTime == null) ? 0 :
+        Math.floor(currentTime / 60);
+    min = min < 10 ? '0' + min : min;
+
+    // define seconds currentTime
+    function get_sec(x) {
+        if (Math.floor(x) >= 60) {
+
+            for (var i = 1; i <= 60; i++) {
+                if (Math.floor(x) >= (60 * i) && Math.floor(x) < (60 * (i + 1))) {
+                    sec = Math.floor(x) - (60 * i);
+                    sec = sec < 10 ? '0' + sec : sec;
+                }
+            }
+        } else {
+            sec = Math.floor(x);
+            sec = sec < 10 ? '0' + sec : sec;
+        }
+    }
+
+    get_sec(currentTime, sec);
+
+    // change currentTime DOM
+    currTime.innerHTML = min + ':' + sec;
+
+    // define minutes duration
+    let min_d = (isNaN(duration) === true) ? '0' :
+        Math.floor(duration / 60);
+    min_d = min_d < 10 ? '0' + min_d : min_d;
+
+
+    function get_sec_d(x) {
+        if (Math.floor(x) >= 60) {
+
+            for (var i = 1; i <= 60; i++) {
+                if (Math.floor(x) >= (60 * i) && Math.floor(x) < (60 * (i + 1))) {
+                    sec_d = Math.floor(x) - (60 * i);
+                    sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
+                }
+            }
+        } else {
+            sec_d = (isNaN(duration) === true) ? '0' :
+                Math.floor(x);
+            sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
+        }
+    }
+
+    // define seconds duration
+
+    get_sec_d(duration);
+
+    // change duration DOM
+    durTime.innerHTML = min_d + ':' + sec_d;
+
+}
+
+
+let updateProgressFn = function(event) {
+    let progress = document.getElementById('progress')
+
+    const { duration, currentTime } = event.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+}
+
+
 
 let userLoginFn = function(event) {
     event.preventDefault();
@@ -421,7 +549,6 @@ function getShuffledSong() {
 
         if (!findInBucket) {
             shuffleBucket.push(selectedSong)
-            console.log(shuffleBucket)
             return selectedSong.id
         }
     }
--- server/data/musics.js
+++ server/data/musics.js
@@ -1,60 +1,60 @@
 const musics = [{
         id: 1,
         title: "Blinding Lights",
-        releaseDate: '2019-01-01',
+        releaseDate: 'November 29, 2019',
         fileName: 'binding-lights.mp3'
     }, {
         id: 2,
         title: "The Twist",
-        releaseDate: '1960-01-01',
+        releaseDate: 'June 1960',
         fileName: 'twist.mp3'
     },
     {
         id: 3,
         title: "Smooth",
-        releaseDate: '1960-01-01',
+        releaseDate: 'June 15, 1999',
         fileName: 'smooth.mp3'
     },
     {
         id: 4,
         title: "Mack the Knife",
-        releaseDate: '1960-01-01',
+        releaseDate: 'December 19, 1958',
         fileName: 'mack-the-knife.mp3'
     },
     {
         id: 5,
         title: "Uptown Funk",
-        releaseDate: '1960-01-01',
+        releaseDate: '30 October 2014',
         fileName: 'uptown-funk.mp3'
     },
     {
         id: 6,
         title: "How Do I Live",
-        releaseDate: '1960-01-01',
+        releaseDate: 'May 23, 1997',
         fileName: 'how-do-i-live.mp3'
     },
     {
         id: 7,
         title: "Party Rock Anthem",
-        releaseDate: '1960-01-01',
+        releaseDate: 'March 9, 2011',
         fileName: 'party-rock-anthem.mp3'
     },
     {
         id: 8,
         title: "I Gotta Feeling",
-        releaseDate: '1960-01-01',
+        releaseDate: 'May 21, 2009',
         fileName: 'I-gotta-feeling.mp3'
     },
     {
         id: 9,
         title: "Macarena (Bayside Boys mix)",
-        releaseDate: '1960-01-01',
+        releaseDate: '15 August 1993',
         fileName: 'macarena.mp3'
     },
     {
         id: 10,
         title: "Shape of You",
-        releaseDate: '1960-01-01',
+        releaseDate: '6 January 2017',
         fileName: 'shape-of-you.mp3'
     },
 
